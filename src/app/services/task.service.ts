76a29ff76c225f3bf6182588bd19cae1ae435cdd
Kyle Wheeless <kyle@wheeless.dev>
2021-12-01 12:48:08 -0800

    add redirects

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -8,7 +8,7 @@ import { Observable, of } from 'rxjs';
 })
 export class TaskService {
   domain = 'https://api.hostmonkey.io/api/v1/links';
-  shortDomain = 'http://localhost:46001';
+  shortDomain = 'http://localhost:46001/api/v1/links';
   getTasks(): Observable<URL[]> {
     return this.http.get<URL[]>(this.domain);
   }
