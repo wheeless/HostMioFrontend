86974d6aa4d1a1a7ca6193621970ff22b46fc0a1
Kyle Wheeless <kyle@wheeless.dev>
2022-03-12 00:38:27 -0800

    made more toast

--- src/app/components/features/features.component.ts
+++ src/app/components/features/features.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { HotToastService } from '@ngneat/hot-toast';
 
 @Component({
   selector: 'app-features',
   templateUrl: './features.component.html',
-  styleUrls: ['./features.component.css']
+  styleUrls: ['./features.component.css'],
 })
 export class FeaturesComponent implements OnInit {
-
-  constructor() { }
+  constructor(private toast: HotToastService) {}
 
   ngOnInit(): void {
+    this.toast.warning(
+      'This page is still under construction, check back later!',
+      {
+        autoClose: true,
+        dismissible: true,
+        position: 'bottom-center',
+      }
+    );
   }
-
 }
--- src/app/components/why-page/why-page.component.ts
+++ src/app/components/why-page/why-page.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { HotToastService } from '@ngneat/hot-toast';
 
 @Component({
   selector: 'app-why-page',
   templateUrl: './why-page.component.html',
-  styleUrls: ['./why-page.component.css']
+  styleUrls: ['./why-page.component.css'],
 })
 export class WhyPageComponent implements OnInit {
-
-  constructor() { }
+  constructor(private toast: HotToastService) {}
 
   ngOnInit(): void {
+    this.toast.warning(
+      'This page is still under construction, check back later!',
+      {
+        autoClose: true,
+        dismissible: true,
+        position: 'bottom-center',
+      }
+    );
   }
-
 }
