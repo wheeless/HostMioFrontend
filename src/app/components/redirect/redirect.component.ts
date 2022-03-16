b376d53b772b438ba07898ee919cabf412efbdf1
Kyle Wheeless <kyle@wheeless.dev>
2022-03-16 00:37:09 -0700

    revert changes until perm fix is deployed

--- src/app/components/redirect/redirect.component.ts
+++ src/app/components/redirect/redirect.component.ts
@@ -16,39 +16,39 @@ export class RedirectComponent implements OnInit {
   URL = 'https://api.hostmonkey.io/api/v1/links/';
   // domainCheck;
   ngOnInit(): void {
-    this.http
-      .head(
-        'https://api.hostmonkey.io/api/v1/links/' +
-          this.route.snapshot.params.shortUrl,
-        {
-          observe: 'response',
-        }
-      )
-      .subscribe((response) => {
-        console.log(response.status);
-        if (response.status === 200) {
-          window.location.href =
-            'https://api.hostmonkey.io/api/v1/links/' +
-            this.route.snapshot.params.shortUrl;
-        } else {
-          window.location.href = 'https://www.hostm.io/404';
-        }
-        // this.domainCheck = data.domain;
-      });
+    // this.http
+    //   .head(
+    //     'https://api.hostmonkey.io/api/v1/links/' +
+    //       this.route.snapshot.params.shortUrl,
+    //     {
+    //       observe: 'response',
+    //     }
+    //   )
+    //   .subscribe((response) => {
+    //     console.log(response.status);
+    //     if (response.status === 200) {
+    //       window.location.href =
+    //         'https://api.hostmonkey.io/api/v1/links/' +
+    //         this.route.snapshot.params.shortUrl;
+    //     } else {
+    //       window.location.href = 'https://www.hostm.io/404';
+    //     }
+    //     // this.domainCheck = data.domain;
+    //   });
 
-    // this.route.params.subscribe((param) => {
-    //   // this.domainCheck =
-    //   //   'https://api.hostmonkey.io/api/v1/links/' + param.shortUrl;
-    //   // if (this.domainCheck === (this.statusCode === NotFound)) {
-    //   // } else {
-    //   // }
-    //   window.location.href =
-    //     'https://api.hostmonkey.io/api/v1/links/' + param.shortUrl;
-    //   // setTimeout(() => {
-    //   //   window.location.href =
-    //   //     'https://api.hostmonkey.io/api/v1/links/' + param.shortUrl;
-    //   // }, 0);
-    //   this.urlShort = param.shortUrl;
-    // });
+    this.route.params.subscribe((param) => {
+      // this.domainCheck =
+      //   'https://api.hostmonkey.io/api/v1/links/' + param.shortUrl;
+      // if (this.domainCheck === (this.statusCode === NotFound)) {
+      // } else {
+      // }
+      window.location.href =
+        'https://api.hostmonkey.io/api/v1/links/' + param.shortUrl;
+      // setTimeout(() => {
+      //   window.location.href =
+      //     'https://api.hostmonkey.io/api/v1/links/' + param.shortUrl;
+      // }, 0);
+      this.urlShort = param.shortUrl;
+    });
   }
 }
