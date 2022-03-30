5387e83c577f47d357166accdc7a059c577a8163
Kyle Wheeless <kyle@wheeless.dev>
2022-03-29 21:31:47 -0700

    correct API path

--- src/app/services/redirect.service.ts
+++ src/app/services/redirect.service.ts
@@ -13,18 +13,18 @@ import { Router } from '@angular/router';
   providedIn: 'root',
 })
 export class RedirectService {
-  //APIHost = 'https://api.hostmonkey.io';
-  APIHost = 'http://localhost:46001';
+  APIHost = 'https://api.hostmonkey.io';
+  //APIHost = 'http://localhost:46001';
 
   APIv1Path = '/api/v1/links/';
   devDomain = 'http://localhost:46001/api/v1/links';
 
   getUrl(shortUrl: string): Observable<URL> {
     return this.http.get<URL>(this.APIHost + this.APIv1Path + shortUrl).pipe(
-      retry(3),
+      retry(1),
       catchError((error) => {
         this.router.navigateByUrl('/404');
-        this.handleError;
+        // this.handleError;
         return throwError(error);
       })
     );
@@ -41,23 +41,23 @@ export class RedirectService {
   //   );
   // }
 
-  private handleError(error: HttpErrorResponse) {
-    if (error.status === 0) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error);
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong.
-      console.error(
-        `Backend returned code ${error.status}, body was: `,
-        error.error
-      );
-    }
-    // Return an observable with a user-facing error message.
-    return throwError(
-      () => new Error('Something bad happened; please try again later.')
-    );
-  }
+  // private handleError(error: HttpErrorResponse) {
+  //   if (error.status === 0) {
+  //     // A client-side or network error occurred. Handle it accordingly.
+  //     console.error('An error occurred:', error.error);
+  //   } else {
+  //     // The backend returned an unsuccessful response code.
+  //     // The response body may contain clues as to what went wrong.
+  //     console.error(
+  //       `Backend returned code ${error.status}, body was: `,
+  //       error.error
+  //     );
+  //   }
+  //   // Return an observable with a user-facing error message.
+  //   return throwError(
+  //     () => new Error('Something bad happened; please try again later.')
+  //   );
+  // }
 
   constructor(private http: HttpClient, private router: Router) {}
 }
