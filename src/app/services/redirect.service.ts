5ba34b0b63ec3d875825d494cd37f79051bb6860
Kyle Wheeless <kyle@wheeless.dev>
2022-04-12 23:04:58 -0700

    added clicks to frontend

--- src/app/services/redirect.service.ts
+++ src/app/services/redirect.service.ts
@@ -18,9 +18,28 @@ export class RedirectService {
   APIv1Path = '/api/v1/links';
   devDomain = 'http://localhost:46001/api/v1/links';
 
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `,
+        error.error
+      );
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(
+      () => new Error('Something bad happened; please try again later.')
+    );
+  }
+
   getUrl(shortUrl: string): Observable<any> {
     const releaseUrl = `${this.APIHost}${this.APIv1Path}`;
     const combinedUrl = `${releaseUrl}/${shortUrl}`;
+    console.log(combinedUrl);
     return this.http.get(combinedUrl).pipe(
       // catchError(this.handleError)
       retry(1),
@@ -41,24 +60,6 @@ export class RedirectService {
   //     })
   //   );
   // }
-
-  private handleError(error: HttpErrorResponse) {
-    if (error.status === 0) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error);
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong.
-      console.error(
-        `Backend returned code ${error.status}, body was: `,
-        error.error
-      );
-    }
-    // Return an observable with a user-facing error message.
-    return throwError(
-      () => new Error('Something bad happened; please try again later.')
-    );
-  }
 
   constructor(private http: HttpClient, private router: Router) {}
 }
